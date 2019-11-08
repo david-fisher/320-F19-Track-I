4f61d00bc3e0e81605d98d2c400d15dbcbfe99a3
Zach Schaffer <zachary.schaffer12@gmail.com>
2019-11-08 15:09:11 -0500

    Fix home page redirect and update gitignore

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,30 +2,27 @@ import React from 'react';
 import logo from '../../assets/images/logo.svg'
 import './App.css';
 
-//router imports
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Router
 import {
   BrowserRouter as Router,
+  Redirect,
   Switch,
   Route,
   Link
 } from "react-router-dom";
 
-//bootstrap imports
-import {Bootstrap, Grid, Row, Col, Nav} from 'react-bootstrap';
 
-import 'bootstrap/dist/css/bootstrap.css';
-//import 'bootstrap/dist/css/bootstrap-theme.css';
 
 function App() {
   return (
     <Router>
       
       <NavBar ></NavBar>
-
       <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
+        <Redirect exact from="/" to="/home" />
         <Route path="/home">
           <Home />
         </Route>
@@ -44,7 +41,7 @@ function App() {
 function NavBar() {
   return <div>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Orchard Watch</a>
+      <a className="navbar-brand" href="/home">Orchard Watch</a>
       
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
