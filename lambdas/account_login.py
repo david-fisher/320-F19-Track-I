5103fe2fd4d0505651d6f692f4da29cce9ba3de2
jbosworth20 <jbosworth@umass.edu>
2019-11-08 19:57:22 -0500

    Converted to be Aurora compatible
    Former-commit-id: 74589ba655604569e8670dfa7502b145609069ac

--- lambdas/account_login.py
+++ lambdas/account_login.py
@@ -1,23 +1,23 @@
 import json
 import boto3
+import psycopg2
 
-def account_login(request_token):
-    stored_username = None #Replace 'None' with getting the request_token from the cloud 
-    #Get password from request_token somehow
-    frontend_password = None #Replace 'None' with getting the password from the request_token
-    #If username does not exist throw a 403 Error
-    if(stored_username == None):
-        return{
+def account_login(frontend_username,frontend_password):
+   #First connect to the table 
+    connection = psycopg.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
+    cursor = connection.cursor()
+
+    #Check if the user exist in the database
+    existing_user = cur.execute("SELECT UserID from User having UserID == requested_UserID") #Not sure I can go and make a variable out of the select
+    if(existing_user == None):
+        return {
             'statusCode': 403 #Forbidden
-        } 
-    #Else get password
-    stored_password = None #Replace 'None' with getting from the cloud 
-    #If passwords do not match throw a 403 Error
-    if(frontend_password != stored_password):
-        return{
-            'statusCode': 403 #Forbidden
-        } 
-    #Else throw a 200 OK
-    return{
-        'statusCode': 200 #OK
-    }
+        }  
+    #Get password from existing user
+    existing_password = cur.execute("SELECT Password from User having UserID == existing_user")
+    if(existing_password == frontend_password):
+        return {
+            'statusCode': 200
+            #Send back a token if okay in valid session table 
+        }
+    return 'statusCode': 403
