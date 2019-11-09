74589ba655604569e8670dfa7502b145609069ac
jbosworth20 <jbosworth@umass.edu>
2019-11-08 19:57:22 -0500

    Converted to be Aurora compatible

--- lambdas/account_registration.py
+++ lambdas/account_registration.py
@@ -1,14 +1,21 @@
-import json
+import psycopg2
 import boto3
+import psycopg
 
-def account_registration(request_token,permission_token):
-    database_user = None #Change this to database.get(request_token) or something like that
-    #If account exist already return Forbidden
-    if(database_user != None ): #Replace 'None' with a way to get 
-        return{
-            'statusCode': 403 #Forbidden
-        }
-    #Else return OKAY
+def registration(requested_UserID,requested_Password,requested_UserType,requested_Name,requested_About):
+   #First connect to the table 
+    connection = psycopg.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
+    cursor = connection.cursor()
+    
+    #Check if the user already exist in the database
+    existing_user = cur.execute("SELECT UserID from User having UserID == requested_UserID") #Not sure I can go and make a variable out of the select
+    if(existing_user != None):
+        return 'statusCode': 403 #Forbidden
+    
+    #If they do not exist in the database, add them
+    cur.execute("INSERT INTO USER (UserID,Email,Password,UserType,Name,About) \
+                 VALUES (requested_UserID,requested_Password,requested_UserType,requested_Name,requested_About)")
     return{
-        'statusCode': 200 #OKAY
+        'statusCode': 403
+        #Create a token for them to use 
     }
--- lambdas/update_password.py
+++ lambdas/update_password.py
@@ -1,15 +1,15 @@
 import json
 import boto3
+import psycopg2
 
-def update_password(new_password):
-    #if user is vertified
-        #Get database
-        #Replace in database
-        #Return 200: OK
+def update_password(userID, new_password):
+    #Assumtion: userID is correct 
+    #First connect to the table 
+    connection = psycopg.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
+    cursor = connection.cursor()
+    
+    #Replace in database
+    cursor.execute("UPDATE Users set Password = new_password where UserID = userID")
     return{
         'statusCode': 200
-    }
-    #else return 403:Forbiddin
-    return{
-        'statusCode': 403
     }
