5ebf402e799403f72923825488e32faf28585a2f
jbosworth20 <jbosworth@umass.edu>
2019-11-14 18:54:41 -0500

    small edits to parameters and connections
    Former-commit-id: 6adfbe1110b3d29ff3b25697a93de54ecfdd8bab

--- lambdas/account_registration.py
+++ lambdas/account_registration.py
@@ -1,22 +1,24 @@
-import psycopg2
 import boto3
 import psycopg2
 
-def registration(requested_UserID,request_Email,requested_Password,requested_UserType,requested_Name,requested_About):
+def registration(requested_UserID,requested_Email,requested_Password,requested_UserType,requested_Name,requested_About):
    #First connect to the table 
-    connection = psycopg.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
+    connection = psycopg2.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
     cursor = connection.cursor()
     
     #Check if the user already exist in the database
     existing_user = cursor.execute("SELECT UserID from User having UserID == requested_UserID") #Not sure I can go and make a variable out of the select
+    connection.commit()
     if(existing_user != None):
         return{
             'statusCode': 403 #Forbidden
         }
     
     #If they do not exist in the database, add them
-    cur.execute("INSERT INTO User (UserID,Email,Password,UserType,Name,About) \
+    cursor.execute("INSERT INTO User (UserID,Email,Password,UserType,Name,About) \
                  VALUES (requested_UserID,requested_Email,requested_Password,requested_UserType,requested_Name,requested_About)")
+    connection.commit()
+    connection.close()
     return{
         'statusCode': 200 #OK
     }
