5250f03aa49e197e8a1cdf229a92db8885fd9a4a
jbosworth20 <jbosworth@umass.edu>
2019-11-14 18:12:07 -0500

    Added Token Functionality and Lambda Handler
    Former-commit-id: a4d8d232c4e3145138132a994bd6f6ab936538ab

--- lambdas/account_login.py
+++ lambdas/account_login.py
@@ -1,23 +1,30 @@
 import json
 import boto3
 import psycopg2
+import uuid
 
-def account_login(frontend_username,frontend_password):
+def account_login(frontend_email,frontend_password):
    #First connect to the table 
     connection = psycopg.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
     cursor = connection.cursor()
 
-    #Check if the user exist in the database
-    existing_user = cur.execute("SELECT UserID from User having UserID == requested_UserID") #Not sure I can go and make a variable out of the select
-    if(existing_user == None):
+    #Check if the user exist in the database and if not return a 400 http 
+    existing_user = cursor.execute("SELECT Email from User having Email == frontend_email") #Not sure I can go and make a variable out of the select
+    if(existing_user == None):#Not sure if returns None if can not find user
         return {
-            'statusCode': 403 #Forbidden
+            'statusCode': 403 #Forbidden 
         }  
-    #Get password from existing user
-    existing_password = cur.execute("SELECT Password from User having UserID == existing_user")
+    
+    #Get password from existing user and if does not match return a 400 http
+    existing_password = cursor.execute("SELECT Password from User having UserID == existing_user")
     if(existing_password == frontend_password):
         return {
-            'statusCode': 200
-            #Send back a token if okay in valid session table 
+            'statusCode': 403 #Forbidden
         }
-    return 'statusCode': 403
+    
+    #Create a token and then push it into the database
+    token = uuid.uuid4()
+    cursor.execute("INSERT INTO Tokens (Email,Token) \ VALUES (frontend_email,token)") #I don't think this table exist 
+    return{
+        'statusCode': 200 #OK
+    }
--- lambdas/update_password.py
+++ lambdas/update_password.py
@@ -2,13 +2,23 @@ import json
 import boto3
 import psycopg2
 
-def update_password(userID, new_password):
-    #Assumtion: userID is correct 
+def update_password(frontend_token, new_password):
+    #Connect to the Token Database
+    connection = psycopg2.connect(database = "Token", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
+    cursor = connection.cursor()
+
+    #Check that token is good
+    existing_token = cursor.execute("SELECT frontend_token from Token having Email == frontend_email")
+    if(existing_token == None):#Not sure if returns None if can not find token
+        return {
+            'statusCode': 403 #Forbidden
+        }  
+    
     #First connect to the table 
-    connection = psycopg.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
+    connection = psycopg2.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
     cursor = connection.cursor()
     
-    #Replace in database
+    #Replace password in database
     cursor.execute("UPDATE Users set Password = new_password where UserID = userID")
     return{
         'statusCode': 200
