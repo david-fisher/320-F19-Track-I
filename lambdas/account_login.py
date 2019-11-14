6adfbe1110b3d29ff3b25697a93de54ecfdd8bab
jbosworth20 <jbosworth@umass.edu>
2019-11-14 18:54:41 -0500

    small edits to parameters and connections

--- lambdas/account_login.py
+++ lambdas/account_login.py
@@ -4,12 +4,13 @@ import psycopg2
 import uuid
 
 def account_login(frontend_email,frontend_password):
-   #First connect to the table 
-    connection = psycopg.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
+   #Connect to the User Database
+    connection = psycopg2.connect(database = "User", user = "postgres",password = "pass123", host = "127.0.0.1", port = "5432")
     cursor = connection.cursor()
 
     #Check if the user exist in the database and if not return a 400 http 
     existing_user = cursor.execute("SELECT Email from User having Email == frontend_email") #Not sure I can go and make a variable out of the select
+    connection.commit() 
     if(existing_user == None):#Not sure if returns None if can not find user
         return {
             'statusCode': 403 #Forbidden 
@@ -17,6 +18,7 @@ def account_login(frontend_email,frontend_password):
     
     #Get password from existing user and if does not match return a 400 http
     existing_password = cursor.execute("SELECT Password from User having UserID == existing_user")
+    connection.commit() 
     if(existing_password == frontend_password):
         return {
             'statusCode': 403 #Forbidden
@@ -24,7 +26,10 @@ def account_login(frontend_email,frontend_password):
     
     #Create a token and then push it into the database
     token = uuid.uuid4()
-    cursor.execute("INSERT INTO Tokens (Email,Token) \ VALUES (frontend_email,token)") #I don't think this table exist 
+    cursor.execute("INSERT INTO Tokens (Email,Token) \
+                 VALUES (frontend_email,token)")
+    connection.commit()
+    connection.close()
     return{
         'statusCode': 200 #OK
     }
