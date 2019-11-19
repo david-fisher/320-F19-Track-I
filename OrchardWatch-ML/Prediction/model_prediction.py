b2614309c3bfd4bd425133708c1c0c690972c51c
Dhruvil Gala <dhruvilgala@1x-nat-vl931-172-30-147-194.wireless.umass.edu>
2019-11-18 20:43:08 -0500

    Connected REST with prediction
    Former-commit-id: 2e8230627d25653216090d0797c978df37d899bd

--- OrchardWatch-ML/Prediction/model_prediction.py
+++ OrchardWatch-ML/Prediction/model_prediction.py
@@ -8,13 +8,8 @@ from keras.applications.imagenet_utils import decode_predictions
 import os
 from keras.models import model_from_json
 from keras.models import load_model
-import sys
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-model_name = sys.argv[1]
-image_url = sys.argv[2]
 
 # model = applications.resnet50.ResNet50(
 #     include_top=True,
@@ -29,21 +24,21 @@ image_url = sys.argv[2]
 # print("Saved model to disk")
 
 # load  model
-loaded_model = load_model(model_name+".h5")
-# print("Loaded model from disk")
+loaded_model = load_model("model.h5")
+print("Loaded model from disk")
 
-# filename = 'cat.jpeg'
+filename = 'cat.jpeg'
 # load an image in PIL format
-original_image = load_img(image_url, target_size=(224, 224))
+original_image = load_img(filename, target_size=(224, 224))
 # convert the PIL image (width, height) to a NumPy array (height, width, channel)
 numpy_image = img_to_array(original_image)
 # Convert the image into 4D Tensor (samples, height, width, channels) by adding an extra dimension to the axis 0.
 input_image = np.expand_dims(numpy_image, axis=0)
 
-# print('PIL image size = ', original_image.size)
-# print('NumPy image size = ', numpy_image.shape)
-# print('Input image size = ', input_image.shape)
-# plt.imshow(np.uint8(input_image[0]))
+print('PIL image size = ', original_image.size)
+print('NumPy image size = ', numpy_image.shape)
+print('Input image size = ', input_image.shape)
+plt.imshow(np.uint8(input_image[0]))
 
 #preprocess for resnet50
 processed_image_resnet50 = applications.resnet50.preprocess_input(input_image.copy())
@@ -51,5 +46,5 @@ processed_image_resnet50 = applications.resnet50.preprocess_input(input_image.co
 # resnet50
 predictions_resnet50 = loaded_model.predict(processed_image_resnet50)
 label_resnet50 = decode_predictions(predictions_resnet50)
-print (label_resnet50)
+print ('label_resnet50 = ', label_resnet50)
 
