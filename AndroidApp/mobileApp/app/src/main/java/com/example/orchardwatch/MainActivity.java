fe1fea6239f87111d5fb1f398439615121e2172c
unknown <dgarkavtseva@umass.edu>
2019-11-30 00:00:32 -0500

    camera button works
    Former-commit-id: c0d19a7a2d7ea55f5d94be817b0593779858de37

--- AndroidApp/mobileApp/app/src/main/java/com/example/orchardwatch/MainActivity.java
+++ AndroidApp/mobileApp/app/src/main/java/com/example/orchardwatch/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.orchardwatch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,13 +18,29 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
+// make it so the buttons dont move when you scroll and make it save camera image
     private WebView my_webview;
-
+    Button camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        camera = (Button)findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivity(intent);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
 //        BottomNavigationView navView = findViewById(R.id.nav_view);
 //        // Passing each menu ID as a set of Ids because each
 //        // menu should be considered as top level destinations.
