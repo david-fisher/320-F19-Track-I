48560fdb7f08d3d60ca5e4a75425bdffbfc1aa45
Jason Xiao <JasonX1250@gmail.com>
2019-12-14 17:36:45 -0500

    changes based on track 1 req doc

--- OrchardWatch-FrontEnd/orchardwatch/src/Components/Login.js
+++ OrchardWatch-FrontEnd/orchardwatch/src/Components/Login.js
@@ -5,23 +5,23 @@ class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      validated: false,
-      alert: { bool: false, message: "" }
+      alert: false,
+      message: ""
     };
   }
 
   render() {
     var alert = <div></div>;
-    if (this.state.alert.bool) {
+    if (this.state.alert) {
       alert = (
         <Alert
           variant="danger"
           onClose={() => {
-            this.setState({ alert: { bool: false, message: "" } });
+            this.setState({ alert: false, message: "" });
           }}
           dismissible
         >
-          {this.state.alert.message}
+          {this.state.message}
         </Alert>
       );
     }
@@ -33,6 +33,9 @@ class Login extends React.Component {
             {/* Register Form */}
             <Col>
               <h3>Register</h3>
+              <div>Growers and researchers need a valid Access Key.</div>
+              <div>Public users can leave the 'Access Key' field blank.</div>
+              <br></br>
               <Form onSubmit={this.validateRegister}>
                 <Form.Row>
                   <Col md="2"></Col>
@@ -89,9 +92,6 @@ class Login extends React.Component {
                   </Col>
                   <Col md="2"></Col>
                 </Form.Row>
-                <div>Growers and researchers need a valid Access Key.</div>
-                <div>Public users can leave the 'Access Key' field blank.</div>
-                <br></br>
                 <Form.Row>
                   <Col md="2"></Col>
                   <Col>
@@ -144,6 +144,14 @@ class Login extends React.Component {
                   </Col>
                   <Col md="2"></Col>
                 </Form.Row>
+                <br></br>
+                <Form.Row>
+                  <Col md="2"></Col>
+                  <Col>
+                    <Button variant="primary">Forgot Password</Button>
+                  </Col>
+                  <Col md="2"></Col>
+                </Form.Row>
               </Form>
             </Col>
           </Row>
@@ -153,67 +161,72 @@ class Login extends React.Component {
   }
 
   validateLogin = e => {
-    const validity = e.currentTarget.checkValidity();
-    if (validity && true) {
-      // replace true with check when sending to Lambdas and wait for authToken upon successful validation or ...
-      // receives boolean based on successful validation, type of user, and authToken if boolean == true
-      // wrong password
-      switch (document.getElementById("LoginEmail").value) {
-        case "public@gmail.com":
-          this.props.auth("public", "Authorized");
-          break;
-        case "grower@gmail.com":
-          this.props.auth("grower", "Authorized");
-          break;
-        case "researcher@gmail.com":
-          this.props.auth("researcher", "Authorized");
-          break;
-        default:
-          this.setState({
-            alert: { bool: true, message: "Wrong Email or Password" }
-          });
-          e.preventDefault();
-          e.stopPropagation();
-          break;
-      }
-    } else {
-      if (validity) {
-        this.setState({
-          alert: { bool: true, message: "Wrong Email or Password" }
-        });
-      }
-      e.preventDefault();
-      e.stopPropagation();
-    }
+    let email = document.getElementById("LoginEmail").value;
+    let password = document.getElementById("LoginPassword").value;
+    fetch(
+      `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/account/login/${email}/${password}/`
+    ).then(response => console.log(response));
+    e.preventDefault();
+    e.stopPropagation();
+    // const validity = e.currentTarget.checkValidity();
+    // console.log(validity);
+    // if (validity && true) {
+    //   // replace true with check when sending to Lambdas and wait for authToken upon successful validation or ...
+    //   // receives boolean based on successful validation, type of user, and authToken if boolean == true
+    //   // wrong password
+    //   switch (document.getElementById("LoginEmail").value) {
+    //     case "public@gmail.com":
+    //       this.props.auth("public", "Authorized");
+    //       break;
+    //     case "grower@gmail.com":
+    //       this.props.auth("grower", "Authorized");
+    //       break;
+    //     case "researcher@gmail.com":
+    //       this.props.auth("researcher", "Authorized");
+    //       break;
+    //     default:
+    //       this.setState({
+    //         alert: true,
+    //         message: "Wrong Email or Password"
+    //       });
+    //       e.preventDefault();
+    //       e.stopPropagation();
+    //       break;
+    //   }
+    // } else {
+    //   if (validity) {
+    //     this.setState({
+    //       alert: true,
+    //       message: "Wrong Email or Password"
+    //     });
+    //   }
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    // }
   };
 
   validateRegister = e => {
-    var validity = e.currentTarget.checkValidity();
-    if (validity) {
-      const p1 = document.getElementById("RegisterPassword").value;
-      const p2 = document.getElementById("RegisterConfirmPassword").value;
-      if (p1.length < 8) {
-        this.setState({
-          alert: {
-            bool: true,
-            message: "Your Password should have more than 8 characters"
-          }
-        });
-        validity = false;
-      } else if (p1 !== p2) {
-        this.setState({
-          alert: { bool: true, message: "The Passwords do not match" }
-        });
-        validity = false;
-      } else {
-        console.log("yes");
-        // send to lambdas
-      }
+    let email = document.getElementById("RegisterEmail").value;
+    let p1 = document.getElementById("RegisterPassword").value;
+    let p2 = document.getElementById("RegisterConfirmPassword").value;
+    let access = document.getElementById("RegisterAccessKey").value;
+    if (p1.length < 8) {
+      this.setState({
+        alert: true,
+        message: "Your Password should have more than 8 characters"
+      });
+    } else if (p1 !== p2) {
+      this.setState({
+        alert: true,
+        message: "The Passwords do not match"
+      });
+    } else {
+      fetch(
+        `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/account/register/${email}/${p1}/${access}`
+      ).then(response => console.log(response));
     }
-    if (!validity) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
+    e.preventDefault();
+    e.stopPropagation();
   };
 }
 
