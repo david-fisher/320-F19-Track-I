a8a68f207b939b499ffe5d14482fae2eba49b2d2
Dhruvil Gala <dhruvilgala@1x-nat-vl931-172-30-155-240.wireless.umass.edu>
2019-12-14 18:07:43 -0500

    s3 model upload

--- OrchardWatch-ML/prediction/model_manager.py
+++ OrchardWatch-ML/prediction/model_manager.py
@@ -4,6 +4,9 @@ import pickle
 import shutil
 import json
 from keras_model import *
+import io
+from PIL import Image
+import boto3
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -52,7 +55,8 @@ def main():
 		else:
 			model_type = model_dict[model_name]
 			if model_type == "keras":
-				predict_keras(os.path.join(model_dir, model_name), img_url)
+				img = load_image(img_url)
+				predict_keras(os.path.join(model_dir, model_name), img)
 
 
 def upload_model(model_types, model_dict, model_url, model_name, model_type):
@@ -60,9 +64,13 @@ def upload_model(model_types, model_dict, model_url, model_name, model_type):
 		raise NameError("Please select either 'keras' or 'pytorch'.")
 
 	# change to boto3 on EC2
-	model_fname = shutil.copyfile(model_url, os.path.join(model_dir, model_name))
+	# model_fname = shutil.copyfile(model_url, os.path.join(model_dir, model_name))
+	model_fname = os.path.join(model_dir, model_name)
+	s3 = boto3.resource('s3', region_name='us-east-2')
+	bucket = s3.Bucket('machine-learning2019')
+	bucket.download_file(model_url, model_fname)
 	
-	if model_fname:
+	if os.path.exists(model_fname):
 		model_dict[model_name] = model_type
 		pickle.dump(model_dict, open(dict_file, "wb"))
 		get_models(model_dict)
@@ -74,14 +82,22 @@ def get_models(model_dict):
 		model_json = json.dumps(model_dict)
 		print(model_json)
 
-# def load_image(img_url):
-# 	s3 = boto3.resource('s3', region_name='us-east-2')
-# 	bucket = s3.Bucket('machine-learning2019')
-# 	object = bucket.Object(img_url)
-# 	file_stream = io.BytesIO()
-# 	object.download_fileobj(file_stream)
-# 	img = mpimg.imread(file_stream)
+def load_image(img_url):
+	# if img_url.startswith('http://') or img_url.startswith('https://') or img_url.startswith('ftp://'):
+	# 	response = requests.get(img_url)
+	# 	img = Image.open(io.BytesIO(response.content))
+	# 	img = img.resize((224, 224))
+	# else:
+	# 	# load an image in PIL format
+	# 	img = load_img(img_url, target_size=(224, 224))
 
+	s3 = boto3.resource('s3', region_name='us-east-2')
+	bucket = s3.Bucket('machine-learning2019')
+	object = bucket.Object(img_url)
+	file_stream = io.BytesIO()
+	object.download_fileobj(file_stream)
+	img = Image.open(file_stream)
+	return img
 
 if __name__== "__main__":
    main()
