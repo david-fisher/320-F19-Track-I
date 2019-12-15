31de9058461c8a7b256754efae47d85c18466995
Eric Teall <teall.eric@gmail.com>
2019-12-15 15:31:42 -0500

    Slight styling changes, wip

--- OrchardWatch-FrontEnd/orchardwatch/src/Components/Classifier_Disp.js
+++ OrchardWatch-FrontEnd/orchardwatch/src/Components/Classifier_Disp.js
@@ -21,9 +21,10 @@ export class ClassDisp extends React.Component
                     <div className="col-12 mt-3">
                         <div className="card">
                             <div className="card-horizontal">
-                                <div className="img-square-wrapper">
-                                    <Image className="" style = {{width: 300, height: 300}} src="http://via.placeholder.com/300x180" alt="Card image cap"/>
+                                <div className="img-square-wrapper" style={{left: 0}}>
+                                    <Image className="" style = {{width: 300, height: 300, borderRadius: 15, borderColor: 'black', borderWidth: 10, left: 0}} src="http://via.placeholder.com/300x180" alt="Card image cap"/>
                                 </div>
+
                                     <h4 className="card-title">Card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make
                                         up the bulk of the card's content.</p>
