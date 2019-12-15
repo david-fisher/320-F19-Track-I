3edc36c5b2b845884e2e03d073b01f859de79d3f
Jason Xiao <JasonX1250@gmail.com>
2019-12-15 02:56:46 -0500

    added additional components for dashboard
    Former-commit-id: 33e3277a27f3e80388a3715320d8562c49b3a681

--- OrchardWatch-FrontEnd/orchardwatch/src/Components/Dashboard.js
+++ OrchardWatch-FrontEnd/orchardwatch/src/Components/Dashboard.js
@@ -1,16 +1,66 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form, Container, Card } from "react-bootstrap";
+import PostAnnouncement from "./PostAnnouncement";
+import UploadImage from "./UploadImage";
+import SystemStatus from "./SystemStatus";
+import Profile from "./Profile";
 
 class Dashboard extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+    this.state = {
+      page: "dashboard"
+    };
+  }
 
-    render() {
-        return(
-            <div></div>
-        );
+  render() {
+    return this.pageRender();
+  }
+
+  pageRender() {
+    let dashboard = (
+      <Container>
+        <Card>
+          <Card.Header onClick={() => this.setState({ page: "profile" })}>
+            Profile
+          </Card.Header>
+        </Card>
+        <Card>
+          <Card.Header
+            onClick={() => this.setState({ page: "postAnnouncement" })}
+          >
+            Post Announcement
+          </Card.Header>
+        </Card>
+        <Card>
+          <Card.Header onClick={() => this.setState({ page: "uploadImage" })}>
+            Upload Image
+          </Card.Header>
+        </Card>
+        <Card>
+          <Card.Header onClick={() => this.setState({ page: "systemStatus" })}>
+            System Status
+          </Card.Header>
+        </Card>
+      </Container>
+    );
+    switch (this.state.page) {
+      case "profile":
+        return <Profile dashboard={this.resetDashboard.bind(this)}/>;
+      case "postAnnouncement":
+        return <PostAnnouncement dashboard={this.resetDashboard.bind(this)}/>;
+      case "uploadImage":
+        return <UploadImage dashboard={this.resetDashboard.bind(this)}/>;
+      case "systemStatus":
+        return <SystemStatus dashboard={this.resetDashboard.bind(this)}/>;
+      default:
+        return <div>{dashboard}</div>;
     }
+  }
+
+  resetDashboard(){
+      this.setState({page: ""});
+  }
 }
 
 export default Dashboard;
