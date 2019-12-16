f0b1e86ccb24854c30ca908afd5d0ac5d78f2369
Annabel Schneider <aeschneider@umass.edu>
2019-12-16 18:27:50 -0500

    added login
    Former-commit-id: 227645dd20a6679f1e556366fc85ca8ba0f5070c

--- iOSOrchardWatch/iOSOrchardWatch/ViewController.swift
+++ iOSOrchardWatch/iOSOrchardWatch/ViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController{
-    
+    @IBOutlet weak var codeinput: UITextField!
+   
     @IBAction func loginButton(_ sender: Any) {
-        performSegue(withIdentifier: "login", sender: self)
+
         //send code to lambda. if good, go to next page. if not, stay on this page with popup saying invalid code
         // /Frontend-Lambda/account/mobile/{code}
+        let url = URL(string: "https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/account/authorization_mobile")!
+        let accesscode:String? = codeinput.text!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let json: [String: Any] = ["code": accesscode]
+        var status = 0;
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    status = response.statusCode
+                    print("statusCode: \(response.statusCode)")
+                }
+                if accesscode != nil, let dataString = accesscode{
+                    print("data: \(dataString)")
+                }
+                if status == 200{
+                    DispatchQueue.main.async { self.performSegue(withIdentifier: "login", sender: self)}
+                }
+                else{
+                    //throw ValidationError("Invalid access code")
+                }
+            }
+        }
+        task.resume()
+        
     }
     
     
