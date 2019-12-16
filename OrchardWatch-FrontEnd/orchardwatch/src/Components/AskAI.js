6be11eb5a3c02ae99034e3358db10e5a4e5ee154
Eric Teall <teall.eric@gmail.com>
2019-12-15 21:20:17 -0500

    required message
    Former-commit-id: ac595169e06e1a428906d81be24dddcb775f8d56

--- OrchardWatch-FrontEnd/orchardwatch/src/Components/AskAI.js
+++ OrchardWatch-FrontEnd/orchardwatch/src/Components/AskAI.js
@@ -8,7 +8,16 @@ class AskAI extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            show: false
+            title: "Object Title",
+            show: false,
+            avatar: "https://ballparkdigest.com/wp-content/uploads/2018/11/Rocky-Mountain-Vibes-300x300.jpg",
+            desc: "Hello I'm testing the first line,\n" +
+                "                                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac\n" +
+                "                                        consectetur ac, vestibulum at eros.Cras mattis consectetur purus sit amet fermentum. Cras justo odio,\n" +
+                "                                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac\n" +
+                "                                        consectetur ac, vestibulum at eros.Cras mattis consectetur purus sit amet fermentum. Cras justo odio,\n" +
+                "                                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta alcapone\n" +
+                "                                        consectetur ac, vestibulum at eros"
         };
 
         this.setModalShow = this.setModalShow.bind(this);
@@ -20,13 +29,14 @@ class AskAI extends React.Component{
             show: newShow
         });
         console.log(this.state);
+
     }
 
     render(){
         return(
             <div>
-                <ClassPopQ key={this.state.show} show={this.state.show} onHide={() => this.setModalShow(false)} />
-                <ClassDisp onClick = {() => this.setModalShow(true)} />
+                <ClassPopQ key={this.state.show} imgSrc = {this.state.avatar} desc = {this.state.desc}  title = {this.state.title} show={this.state.show} onHide={() => this.setModalShow(false)} />
+                <ClassDisp imgSrc = {this.state.avatar} title = {this.state.title} desc = {this.state.desc} onClick = {() => this.setModalShow(true)} />
             </div>
         );
     }
