b40c45cac90b28a855e279e86d5672a06b195d08
Eric Teall <teall.eric@gmail.com>
2019-12-16 18:11:13 -0500

    OK BIG UPDATE:
    Got multiple classifiers loaded
    
    Made an API test stub for my local machine.
    
    Fixed my modal display and made uploading on a picture work (five hours over two days over a vanity thing.)
    
    Deleting classifiers works and updates the list.
    
    No way to add classifiers yet.
    
    Need to figure out how to send and receive api for actually generating classification results.
    
    Need to fix the background scrolling.
    
    Everyone can delete classifiers.
    
    I googled everything and asked very loaded questions on reddit.
    
    I got yelled at by a professional developer for poor state management.
    
    I like CSS.
    
    
    Former-commit-id: 3bef26286f7a9618b7d255ca90a7ac561bdfd43c

--- OrchardWatch-FrontEnd/orchardwatch/src/Components/classPopQ.js
+++ OrchardWatch-FrontEnd/orchardwatch/src/Components/classPopQ.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import {Modal, Button, ButtonToolbar, Image} from "react-bootstrap";
+import camGraphic from './PrettyEricGraphics/classificationUpload.png';
 import "./classModal.css"
-import camGraphic from "./PrettyEricGraphics/classificationUpload.png";
-
 
 function MyVerticallyCenteredModal(props) {
+    console.log(props);
     return (
         <Modal
             {...props}
@@ -24,9 +24,15 @@ function MyVerticallyCenteredModal(props) {
             <Modal.Body>
             <div className= "imgWrappers">
                 <Image className="classAvatar"  src={props.imgSrc} alt="Card image cap"/>
-                <div className= "button">
-                    <input type='file'   onChange={props.onChange} />
+
+                <div className= "button" primary = {false} onClick={(e)=>{props.openFilePicker(e)}}>
+                    <Image className="uploadBase"   src={props.imgPreview || camGraphic} />
                 </div>
+
+                <input type='file' id = 'filePicker'
+                       onChange={(e) => props.upload(e)}
+                       accept="image/*"
+                       style={{display: "none"}} />
             </div>
 
                 <h4>Centered Modal</h4>
@@ -45,26 +51,47 @@ class ClassPopQ extends React.Component {
 
     constructor(props){
         super(props);
+        this.openFilePicker = (e) => this.onOpenFilePicker(e);
+        this.handleClick = (e) =>  this.onChangeFile(e);
         this.state = {
             imgSrc: props.imgSrc,
             desc: props.desc,
             onHide: props.onHide,
             title: props.title,
-            show: props.show
+            imgPreview: null
         }
     }
 
+    onOpenFilePicker(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        // Can possible use React refs
+        const picker = document.getElementById('filePicker');
+        picker.click();
+    }
 
+    onChangeFile(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        let file = e.target.files[0];
+        console.log(file);
+        this.setState({
+            imgPreview: URL.createObjectURL(file)
+        }); /// if you want to upload latter
+    }
     render() {
 
         return (
             <ButtonToolbar>
                 <MyVerticallyCenteredModal
-                    imgSrc={this.state.imgSrc}
-                    show={this.state.show}
-                    onHide={this.state.onHide}
-                    title={this.state.title}
-                    desc = {this.state.desc}
+                    imgSrc ={this.state.imgSrc}
+                    imgPreview = {this.state.imgPreview}
+                    show   ={this.props.show}
+                    onHide ={this.state.onHide}
+                    title  ={this.state.title}
+                    desc   ={this.state.desc}
+                    upload ={this.handleClick}
+                    openFilePicker = {this.openFilePicker}
                 />
             </ButtonToolbar>
         );
