ec7262d3ae94d9b021f37c9dbdcfe193341decb5
unknown <dennis_yatsula@yahoo.com>
2019-12-16 00:01:34 -0500

    ml tests

--- OrchardWatch-ML/ml_server/request_tests.py
+++ OrchardWatch-ML/ml_server/request_tests.py
@@ -4,37 +4,37 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
     def test_predict_missingArgs(self):
-        url = "http://localhost:3000/predict"
+        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/predict"
         response = requests.get(url)
         self.assertTrue(response.status_code == 412)
 
     def test_predict(self):
-        url = "ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/predict?modelName=uploaded_model.h5&imageURL=test_images/dog.jpeg"
+        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/predict?modelName=resnet_keras.h5&imageURL=test_images/dog.jpeg"
         response = requests.get(url)
         self.assertTrue(response.status_code == 200)
 
     def test_upload_new_missingArgs(self):
-        url = "http://localhost:3000/upload/new"
+        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/upload/new"
         response = requests.post(url)
         self.assertTrue(response.status_code == 412)
-
+    
     def test_upload_new(self):
-        url = "http://localhost:3000/upload/new?modelName=model"
+        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/upload/new?modelFileURL=test_models/resnet50_keras.h5&modelName=test.h5&modelType=keras"
         response = requests.post(url)
         self.assertTrue(response.status_code == 200)
-
+    
     def test_upload_replace_missingArgs(self):
-        url = "http://localhost:3000/upload/replace"
+        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/upload/replace"
         response = requests.put(url)
         self.assertTrue(response.status_code == 412)
 
     def test_upload_replace(self):
-        url = "http://localhost:3000/upload/replace?modelName=model"
+        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/upload/replace?modelFileURL=test_models/resnet50_keras.h5&modelName=test.h5&modelType=keras"
         response = requests.put(url)
         self.assertTrue(response.status_code == 200)
     
     def test_list_models(self):
-        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com/models/list"
+        url = "http://ec2-3-18-109-238.us-east-2.compute.amazonaws.com:3000/models/list"
         response = requests.get(url)
         self.assertTrue(response.status_code == 200)
 
