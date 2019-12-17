406dcd47621e33b95d50cd92752ae562d92b9a82
Jason Xiao <JasonX1250@gmail.com>
2019-12-17 17:42:41 -0500

    navbar shows up front now

--- OrchardWatch-FrontEnd/orchardwatch/src/Components/Data.js
+++ OrchardWatch-FrontEnd/orchardwatch/src/Components/Data.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Table, Row, Col, Button } from 'react-bootstrap';
-import DateRangePicker from 'react-daterange-picker';
-import 'react-daterange-picker/dist/css/react-calendar.css';
-import originalMoment from 'moment';
-import { extendMoment } from 'moment-range';
+import React from "react";
+import { Table, Row, Col, Button } from "react-bootstrap";
+import DateRangePicker from "react-daterange-picker";
+import "react-daterange-picker/dist/css/react-calendar.css";
+import originalMoment from "moment";
+import { extendMoment } from "moment-range";
 
 const moment = extendMoment(originalMoment);
 
@@ -15,44 +15,43 @@ class Data extends React.Component {
       date: moment.range(
         moment()
           .clone()
-          .subtract(7, 'days'),
+          .subtract(7, "days"),
         moment().clone()
-      ),
-      data: []
+      )
     };
   }
 
   // implement dropdown to select which orchard to view data of
-  componentDidMount() {
-    this.setState({
-      data: [
-        {
-          Date: '11/12/2018',
-          Time: '12:00',
-          'Tempertaure (F)': 84.32,
-          'Wind Speed': 1.5,
-          'Wetness (%)': 7.6,
-          'Rain (in.)': 0
-        },
-        {
-          Date: '11/13/2018',
-          Time: '12:00',
-          'Tempertaure (F)': 83.24,
-          'Wind Speed': 0.7,
-          'Wetness (%)': 7.1,
-          'Rain (in.)': 0
-        },
-        {
-          Date: '11/14/2018',
-          Time: '12:00',
-          Tempertaure: 84.95,
-          'Wind Speed': 3,
-          'Wetness (%)': 7.6,
-          'Rain (in.)': 0
-        }
-      ]
-    });
-  }
+  // componentDidMount() {
+  //   this.setState({
+  //     data: [
+  //       {
+  //         Date: "11/12/2018",
+  //         Time: "12:00",
+  //         "Tempertaure (F)": 84.32,
+  //         "Wind Speed": 1.5,
+  //         "Wetness (%)": 7.6,
+  //         "Rain (in.)": 0
+  //       },
+  //       {
+  //         Date: "11/13/2018",
+  //         Time: "12:00",
+  //         "Tempertaure (F)": 83.24,
+  //         "Wind Speed": 0.7,
+  //         "Wetness (%)": 7.1,
+  //         "Rain (in.)": 0
+  //       },
+  //       {
+  //         Date: "11/14/2018",
+  //         Time: "12:00",
+  //         Tempertaure: 84.95,
+  //         "Wind Speed": 3,
+  //         "Wetness (%)": 7.6,
+  //         "Rain (in.)": 0
+  //       }
+  //     ]
+  //   });
+  // }
 
   onSelect = (date, states) => {
     this.setState({ date, states });
@@ -65,7 +64,10 @@ class Data extends React.Component {
           <p>
             <b>Pick a date range to query for data</b>
           </p>
-          <p>{this.state.date.start.format('MM/DD/YYYY')} to {this.state.date.end.format('MM/DD/YYYY')}</p>
+          <p>
+            {this.state.date.start.format("MM/DD/YYYY")} to{" "}
+            {this.state.date.end.format("MM/DD/YYYY")}
+          </p>
           <DateRangePicker
             value={this.state.date}
             onSelect={this.onSelect}
@@ -83,15 +85,37 @@ class Data extends React.Component {
         </div>
       );
     } else {
-      // fetch(`https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/data/data_download/{token}/${this.state.date.start.format('MM/DD/YYYY')}/${this.state.date.end.format('MM/DD/YYYY')}/{field}`)
-      // .then(response => {console.log(response)});
-      if (this.state.data.length === 0) {
+      let data = fetch(
+        `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/data/data_download/${this.state.date.start.format(
+          "YYYY-MM-DD"
+        )}/${this.state.date.end.format("YYYY-MM-DD")}`,
+        // `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/data/data_download/1970-01-01/2000-12-20`,
+        {
+          method: "GET"
+        }
+      )
+        .then(response => {
+          if (response.status === 200) {
+            return response.json();
+          } else {
+            return [];
+          }
+        })
+        .then(result => {
+          if (result.length === 0) {
+            return [];
+          } else {
+            console.log(result[0]);
+            return result['0'];
+          }
+        });
+      if (data.length === 0) {
         return <div>No Data Available</div>;
       }
-      let rows = Object.keys(this.state.data[0]).map((e, index) => {
+      let rows = Object.keys(data[0]).map((e, index) => {
         return <th key={index}>{e}</th>;
       });
-      let data = this.state.data.map((e1, index1) => {
+      let dataTable = data.map((e1, index1) => {
         let rows = Object.keys(e1).map((e2, index2) => {
           return <td key={index2}>{e1[e2]}</td>;
         });
@@ -99,35 +123,37 @@ class Data extends React.Component {
       });
       let table = (
         <Row>
-          <Col md='2' />
+          <Col md="2" />
           <Col>
             <Table striped bordered hover>
               <thead>
                 <tr>{rows}</tr>
               </thead>
-              <tbody>{data}</tbody>
+              <tbody>{dataTable}</tbody>
             </Table>
           </Col>
-          <Col md='2' />
+          <Col md="2" />
         </Row>
       );
       return (
         <div>
           {table}
-          <Button onClick={() => this.downloadData(this.state.data)}>Download Data</Button>
+          <Button onClick={() => this.downloadData(data)}>Download Data</Button>
           <br></br>
           <br></br>
-          <Button onClick={() => this.setState({select: true})}>Go Back</Button>
+          <Button onClick={() => this.setState({ select: true })}>
+            Go Back
+          </Button>
         </div>
       );
     }
   }
 
-  downloadData(data){
-    const { Parser } = require('json2csv');
-    const parser = new Parser({eol: ";"});
+  downloadData(data) {
+    const { Parser } = require("json2csv");
+    const parser = new Parser({ eol: ";" });
     let csv = parser.parse(data);
-    console.log(csv);
+    // console.log(csv);
   }
 }
 
