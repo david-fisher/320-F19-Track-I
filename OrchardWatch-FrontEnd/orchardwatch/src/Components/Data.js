40d7c0434fed0dbc1de0fd42613e004718522dc8
Jason Xiao <JasonX1250@gmail.com>
2019-12-17 18:51:16 -0500

    can download data now

--- OrchardWatch-FrontEnd/orchardwatch/src/Components/Data.js
+++ OrchardWatch-FrontEnd/orchardwatch/src/Components/Data.js
@@ -12,46 +12,16 @@ class Data extends React.Component {
     super();
     this.state = {
       select: true,
+      search: false,
       date: moment.range(
         moment()
           .clone()
           .subtract(7, "days"),
         moment().clone()
-      )
+      ),
+      data: []
     };
   }
-
-  // implement dropdown to select which orchard to view data of
-  // componentDidMount() {
-  //   this.setState({
-  //     data: [
-  //       {
-  //         Date: "11/12/2018",
-  //         Time: "12:00",
-  //         "Tempertaure (F)": 84.32,
-  //         "Wind Speed": 1.5,
-  //         "Wetness (%)": 7.6,
-  //         "Rain (in.)": 0
-  //       },
-  //       {
-  //         Date: "11/13/2018",
-  //         Time: "12:00",
-  //         "Tempertaure (F)": 83.24,
-  //         "Wind Speed": 0.7,
-  //         "Wetness (%)": 7.1,
-  //         "Rain (in.)": 0
-  //       },
-  //       {
-  //         Date: "11/14/2018",
-  //         Time: "12:00",
-  //         Tempertaure: 84.95,
-  //         "Wind Speed": 3,
-  //         "Wetness (%)": 7.6,
-  //         "Rain (in.)": 0
-  //       }
-  //     ]
-  //   });
-  // }
 
   onSelect = (date, states) => {
     this.setState({ date, states });
@@ -77,19 +47,19 @@ class Data extends React.Component {
           <br></br>
           <Button
             onClick={() => {
-              this.setState({ select: false });
+              this.setState({ select: false, search: true });
             }}
           >
             Confirm
           </Button>
         </div>
       );
-    } else {
+    } else if (this.state.search) {
       let data = fetch(
-        `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/data/data_download/${this.state.date.start.format(
-          "YYYY-MM-DD"
-        )}/${this.state.date.end.format("YYYY-MM-DD")}`,
-        // `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/data/data_download/1970-01-01/2000-12-20`,
+        // `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/data/data_download/${this.state.date.start.format(
+        //   "YYYY-MM-DD"
+        // )}/${this.state.date.end.format("YYYY-MM-DD")}`,
+        `https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/Frontend-Lambda/data/data_download/1970-01-01/2000-12-20`,
         {
           method: "GET"
         }
@@ -102,58 +72,84 @@ class Data extends React.Component {
           }
         })
         .then(result => {
-          if (result.length === 0) {
-            return [];
-          } else {
-            console.log(result[0]);
-            return result['0'];
+          if (result.length !== 0) {
+            console.log(result["0"]);
+            this.setState({
+              data: [
+                {
+                  Date: "11/12/2018",
+                  Time: "12:00",
+                  "Tempertaure (F)": 84.32,
+                  "Wind Speed": 1.5,
+                  "Wetness (%)": 7.6,
+                  "Rain (in.)": 0
+                },
+                {
+                  Date: "11/13/2018",
+                  Time: "12:00",
+                  "Tempertaure (F)": 83.24,
+                  "Wind Speed": 0.7,
+                  "Wetness (%)": 7.1,
+                  "Rain (in.)": 0
+                },
+                {
+                  Date: "11/14/2018",
+                  Time: "12:00",
+                  Tempertaure: 84.95,
+                  "Wind Speed": 3,
+                  "Wetness (%)": 7.6,
+                  "Rain (in.)": 0
+                }
+              ]
+            });
           }
+          this.setState({ search: false });
         });
-      if (data.length === 0) {
-        return <div>No Data Available</div>;
-      }
-      let rows = Object.keys(data[0]).map((e, index) => {
-        return <th key={index}>{e}</th>;
-      });
-      let dataTable = data.map((e1, index1) => {
-        let rows = Object.keys(e1).map((e2, index2) => {
-          return <td key={index2}>{e1[e2]}</td>;
-        });
-        return <tr key={index1}>{rows}</tr>;
-      });
-      let table = (
-        <Row>
-          <Col md="2" />
-          <Col>
-            <Table striped bordered hover>
-              <thead>
-                <tr>{rows}</tr>
-              </thead>
-              <tbody>{dataTable}</tbody>
-            </Table>
-          </Col>
-          <Col md="2" />
-        </Row>
-      );
-      return (
-        <div>
-          {table}
-          <Button onClick={() => this.downloadData(data)}>Download Data</Button>
-          <br></br>
-          <br></br>
-          <Button onClick={() => this.setState({ select: true })}>
-            Go Back
-          </Button>
-        </div>
-      );
     }
+    if (this.state.data.length === 0) {
+      return <div>No Data Available</div>;
+    }
+    let rows = Object.keys(this.state.data[0]).map((e, index) => {
+      return <th key={index}>{e}</th>;
+    });
+    let dataTable = this.state.data.map((e1, index1) => {
+      let rows = Object.keys(e1).map((e2, index2) => {
+        return <td key={index2}>{e1[e2]}</td>;
+      });
+      return <tr key={index1}>{rows}</tr>;
+    });
+    let table = (
+      <Row>
+        <Col md="2" />
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>{rows}</tr>
+            </thead>
+            <tbody>{dataTable}</tbody>
+          </Table>
+        </Col>
+        <Col md="2" />
+      </Row>
+    );
+    return (
+      <div>
+        {table}
+        <Button href={this.downloadData(this.state.data)} download="data.csv">
+          Download Data
+        </Button>
+        <br></br>
+        <br></br>
+        <Button onClick={() => this.setState({ select: true })}>Go Back</Button>
+      </div>
+    );
   }
 
   downloadData(data) {
     const { Parser } = require("json2csv");
-    const parser = new Parser({ eol: ";" });
-    let csv = parser.parse(data);
-    // console.log(csv);
+    const parser = new Parser();
+    let csv = encodeURI("data:text/csv;charset=utf-8," + parser.parse(data));
+    return csv;
   }
 }
 
