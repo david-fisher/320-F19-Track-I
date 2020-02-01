565156c9f575f8bb31bd32b41210c22608d2adfc
Zach Schaffer <cursedhydra@gmail.com>
2020-02-01 16:34:18 -0500

    import Track 2's front end and fix warnings
    Former-commit-id: df27b595afb8e2bdf0c6877b19bd266ee237ebf4

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,32 +1,60 @@
 import React from "react";
 import "./About.css";
 import { SocialIcon } from "react-social-icons";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Timeline } from "react-twitter-widgets";
 
-class About extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export function About() {
+  return (
+    <body>
+      <Container className="Title">
+        <h1>About Us</h1>
+        <hr />
+      </Container>
+      <Container>
+        <img src={require("./orchard.jpg")} alt="Orchard" class="center" />
 
-  render() {
-    return (
-      <div>
-        <Container className="Title">
-          <h1>About us</h1>
-          <hr></hr>
-        </Container>
-
-        <Container>
-          <h2>Follow us on social media:</h2>
-          <Row>
-            <SocialIcon url="http://facebook.com" />
-            <SocialIcon url="http://instagram.com" />
-            <SocialIcon url="http://twitter.com" />
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+        <h1 className="header ">Our Mission</h1>
+        <p class="lead border border-light rounded">
+          Here at OrchardWatch, we are working to help farmers be more efficient
+          with production and management on their farms while also bringing
+          knowledge about where our food comes from into the public eye. Today
+          there are over 820 million people suffering from hungering around the
+          world. With the global population set to surpass 9 billion by the year
+          2050, our food production will need to more than double. This
+          increased demand for food production, combined with the growing threat
+          of climate change affecting our crops, means that farms will need to
+          use their land more efficiently in order to put an end to world
+          hunger. By using networks of sensors and advanced models, we can
+          provide meaningful and precise data to help farmers better visualize
+          conditions on their farms, receive alerts when plants are at risk, and
+          predict when the best time to fertilize, water, and harvest will be.
+          We also give farmers the tools they need to identify pests and
+          diseases that may hurt their crops by using artificial intelligence to
+          analyze images and identify possible threats. It is our hope that by
+          raising awareness and improving the efficiency of farms we can create
+          a better future without mass hunger.
+        </p>
+      </Container>
+      <Container className="timeline">
+        <Timeline
+          dataSource={{
+            sourceType: "profile",
+            screenName: "BarackObama"
+          }}
+          options={{
+            height: "600"
+          }}
+        />
+      </Container>
+      <footer class="page-footer font-small bg-dark footer navbar-fixed-bottom sticky">
+        Follow Our Social Media
+        <SocialIcon className="left-margin" url="http://facebook.com" />
+        <SocialIcon url="http://instagram.com" />
+        <SocialIcon url="http://twitter.com" />
+      </footer>
+    </body>
+  );
 }
 
 export default About;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,13 +1,45 @@
 import React from "react";
 import Cookies from "universal-cookie";
+import { Container, Card, Col } from "react-bootstrap";
 
 export default function Profile() {
-    const cookies = new Cookies();
-    return (
-        <div>
-            <h2>This is the profile page.</h2>
-    <h3>Name: {cookies.get('name')}</h3>
-        </div>
-    );
+  const cookies = new Cookies();
+  const firstName = cookies.get("firstName");
+  const lastName = cookies.get("lastName");
+  const email = cookies.get("email");
+  const role = cookies.get("role");
 
+  return (
+    <div className="page">
+      <Container className="Title">
+        <h1>Your Profile {cookies.get("name")}</h1>
+        <hr className="titleHR" />
+      </Container>
+      <Container>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Profile Information</Card.Title>
+              <hr />
+              <Card.Text className="row-css">First Name</Card.Text>
+              <p>{firstName}</p>
+              <hr />
+              <Card.Text className="row-css">Last Name</Card.Text>
+              <p>{lastName}</p>
+              <hr />
+              <Card.Text>Password</Card.Text>
+              <p>*****************</p>
+              <a href="/home">Change your password</a>
+              <hr />
+              <Card.Text>Email</Card.Text>
+              <p>{email}</p>
+              <hr />
+              <Card.Text>Birthday</Card.Text>
+              <p>CHANGEME</p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Container>
+    </div>
+  );
 }
