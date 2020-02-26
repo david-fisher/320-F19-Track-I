3fcac4a0510a1c1bfa96185abb048d1b6d2b09c9
Akash Munjial <amunjial@umass.edu>
2020-02-26 11:00:36 -0500

    React App
    Former-commit-id: 3efdc93c3241867d5e0c6a63b93f109f5ebc402b

--- orchardWatchReact/App.js
+++ orchardWatchReact/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions, Button, Image } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TextInput, Dimensions, Button, Image } from 'react-native';
 
 
-export default function App() {
+export default class App extends Component {
 
-  return (
-    <View style={styles.container}>
-      <Image
-        source = {require('./assets/orchardWatch.png')}
-      />
-      <TextInput 
-        style = {styles.inputBox}
-        placeholder = 'Access Code'
-        secureTextEntry = {true}
-        keyboardType = 'number-pad'
-        textAlign = {'center'}
-        placeholderTextColor = {'gray'}
-      />
-      <Button
-        style = {styles.submitButton}
-        title = 'Submit'
-      />
-    </View>
-  );
+  code = ''
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style = {{height: 200, width: Dimensions.get('window').width - 40}}
+          source = {require('./assets/orchardWatch.png')}
+        />
+        <TextInput 
+          style = {styles.inputBox}
+          placeholder = 'Access Code'
+          secureTextEntry = {true}
+          keyboardType = 'number-pad'
+          textAlign = {'center'}
+          placeholderTextColor = {'gray'}
+          onChangeText = {(text)=>this.setState({code: text})}
+        />
+        <Button
+          style = {styles.submitButton}
+          title = 'Submit'
+          onPress = {()=>{console.log(code)}}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -39,3 +45,5 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
 });
+
+AppRegistry.registerComponent('App',()=>'App');
