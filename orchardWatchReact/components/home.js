c4aef5d9bec2160b965c1ef62da1200a3fb2be29
Joey Donovan <joey@Joeys-MacBook-Pro.local>
2020-02-26 22:42:52 -0500

    changed App.js to handle page navigation

--- orchardWatchReact/components/home.js
+++ orchardWatchReact/components/home.js
@@ -16,19 +16,4 @@ export default class Home extends Component {
         </View>
     )
   }
-//   HomeScreen({ navigation }) {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Home Screen</Text>
-//         <Button
-//           title="Go to Details"
-//           onPress={() => navigation.navigate('Login')}
-//         />
-//       </View>
-//     );
-//   }
 }
-
-// const styles = StyleSheet.create({
-    
-// })
