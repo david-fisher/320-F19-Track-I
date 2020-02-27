57ac2e10b6ec4000b3d61c3036532501c456a46c
Joey Donovan <joey@Joeys-MacBook-Pro.local>
2020-02-26 22:42:52 -0500

    changed App.js to handle page navigation
    Former-commit-id: c4aef5d9bec2160b965c1ef62da1200a3fb2be29

--- orchardWatchReact/App.js
+++ orchardWatchReact/App.js
@@ -1,60 +1,33 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, TextInput, Dimensions, Button, Image, Linking } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TextInput, Dimensions, Button, Image } from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, createSwitchNavigator} from '@react-navigation/stack';
+import Home from './components/home';
+import Login from './components/login';
 
-
+const Stack = createStackNavigator();
 
 export default class App extends Component {
-
-  state = {
-    code: -1
-  }
-  LoginHandler() {
-    if (this.state.code === '0000') {
-      
-    }
-    else {
-      alert("Incorrect Access Code")
-    }
-  }
   render() {
     return (
-      <View style={styles.container}>
-        <Image
-          style = {{height: 200, width: Dimensions.get('window').width - 40}}
-          source = {require('./assets/orchardWatch.png')}
-        />
-        <TextInput 
-          style = {styles.inputBox}
-          placeholder = 'Access Code'
-          secureTextEntry = {true}
-          keyboardType = 'number-pad'
-          textAlign = {'center'}
-          placeholderTextColor = {'gray'}
-          onChangeText = {(code)=>this.setState({code})}
-        />
-        <Button
-          style = {styles.submitButton}
-          title = 'Submit'
-          onPress = {()=>this.LoginHandler()}
-        />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ gestureEnabled: false }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ title: 'Login' }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: 'Homepage' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#90EE90',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputBox: {
-    height: 40, 
-    width: Dimensions.get('window').width - 40, 
-    borderColor: 'black', 
-    borderWidth: 1,
-  },
-});
-
-AppRegistry.registerComponent('App',()=>App);
