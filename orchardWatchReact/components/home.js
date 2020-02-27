a87272f66a1e97a9cd47dc541d67d4300eb78b98
Joey Donovan <joey@Joeys-MacBook-Pro.local>
2020-02-26 22:36:20 -0500

    added separate files for home and login pages

---
+++ orchardWatchReact/components/home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, TextInput, Dimensions, Button, Image } from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, createSwitchNavigator} from '@react-navigation/stack';
+
+export default class Home extends Component {
+  render() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+            title="Go to datasets page"
+            onPress={() => this.props.navigation.navigate('Login')}
+                />
+        </View>
+    )
+  }
+//   HomeScreen({ navigation }) {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Home Screen</Text>
+//         <Button
+//           title="Go to Details"
+//           onPress={() => navigation.navigate('Login')}
+//         />
+//       </View>
+//     );
+//   }
+}
+
+// const styles = StyleSheet.create({
+    
+// })
