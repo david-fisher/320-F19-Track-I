c002a3bfe76a76922868ed944cd29923472919f4
khesemeyerumass <60161543+khesemeyerumass@users.noreply.github.com>
2020-03-22 21:18:09 -0400

    Contacts Page Displays Data
    Got the page to display json data
    
    
    Former-commit-id: 4af1e8f7ec913eb37b1697b2b803d691c62263ad

--- client/src/pages/Contacts/Contacts.js
+++ client/src/pages/Contacts/Contacts.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Contacts.css";
 import { Container } from "react-bootstrap";
+import ContactsData from './contacts.json'
+
+// This function will be used in order to get the list of contacts that will be displayed
+// on the contacts page. At the moment, it will simply return a list of researcher and 
+// grower contact information. In the future, it will get this information using a GET 
+// request on a specific endpoint.
+function getContactsList() {
+	//return testList;
+}
 
 export function Contacts() {
   return (
@@ -9,27 +18,18 @@ export function Contacts() {
         <h1>Contact Information</h1>
         <hr />
       </Container>
-      <Container>
-        <h1 className="name">Farmer Bob</h1>
-        <p class="lead border border-light rounded">
-          Email: farmersUnited@gmail.com
-        </p>
-        
-        <h1 className="name">Researcher Phil</h1>
-        <p class="lead border border-light rounded">
-          Email: researchersUnited@gmail.com
-        </p>
-        
-        <h1 className="name">Donut Monkey</h1>
-        <p class="lead border border-light rounded">
-          Email: giveMeDonuts@aol.com
-        </p>
-        
-        <h1 className="name">Dirty Dan</h1>
-        <p class="lead border border-light rounded">
-          Email: imNotPinheadLarry@aol.com
-        </p>
-      </Container>
+      
+      {ContactsData.map((contactsInfo, index)=> {
+        return <Container>
+        	<h1 classname="name">{contactsInfo.name}</h1>
+        	<p class="lead border border-light rounded">
+        	  {contactsInfo.position}
+        	</p>
+        	<p class="lead border border-light rounded">
+        	  {contactsInfo.email}
+        	</p>
+        </Container>
+      })}
     </body>
   );
 }
