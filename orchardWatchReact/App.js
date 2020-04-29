578b8189b69face789b8097d04e6e3947e024e93
Annabel Schneider <aeschneider@umass.edu>
2020-04-29 19:53:10 -0400

    Revert "Merge pull request #111 from david-fisher/Fantastic4"
    This reverts commit 67d8d283d4c42066f5194c98064121d3a01b8114, reversing
    changes made to 34e08fd8937dbee2ec6cb756727d77462aa265ca.

--- orchardWatchReact/App.js
+++ orchardWatchReact/App.js
@@ -1,91 +1,60 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, TextInput, Dimensions, Button, Image, StatusBar, AsyncStorage } from 'react-native';
-import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator, createSwitchNavigator} from '@react-navigation/stack';
-import Home from './components/home';
-import Login from './components/login';
-import Datasets from './components/datasets';
-import { Cookies } from "react-cookie";
-import CameraWindow from './components/camera.window'
-import Gallery from "./components/gallery";
-import TableEntry from "./components/TabularEntry";
-import ExistDataset from "./components/exist_dataset";
-import * as ScreenOrientation from 'expo-screen-orientation';
+import { AppRegistry, StyleSheet, Text, View, TextInput, Dimensions, Button, Image } from 'react-native';
 
 
-const Stack = createStackNavigator();
-// const cookies = new Cookies();
+
 export default class App extends Component {
 
-  async componentDidMount() {
-    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  state = {
+    code: -1
   }
-  // authentication = Cookies.get('authenticated')
-  setRoute = async () => {
-    // if (cookies.get('authenticated',{path: '/'}) === true) {
-    //   return 'Home'
-    // } else {
-    //   return 'Login'
-    // }
-    try {
-      const authenticated = await AsyncStorage.getItem('authenticated')
-      if (authenticated === null) {
-        return 'Login'
-      }
-      else {
-        return 'Home'
-      }
-    } catch (error) {
-      alert(error)
+  LoginHandler() {
+    if (this.state.code === '0000') {
+      
+    }
+    else {
+      alert("Incorrect Access Code")
     }
   }
-
   render() {
-    console.disableYellowBox = true
     return (
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName= {this.setRoute}
-          screenOptions={{ gestureEnabled: false }}
-        >
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{ title: 'Login', headerShown: false}}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ title: 'Homepage', headerShown: false }}
-          />
-          <Stack.Screen
-            name="Datasets"
-            component={Datasets}
-            options={{ title: 'Datasets', headerShown: true}}
-          />
-          <Stack.Screen
-            name="CameraPage"
-            component={CameraWindow}
-            options={{ title: 'Camera', headerShown: false }}
-          />
-          <Stack.Screen
-            name="Gallery"
-            component={Gallery}
-            options={{ title: 'Gallery', headerShown: false }}
-          />
-          <Stack.Screen
-            name="TabularEntry"
-            component={TableEntry}
-            options={{ title: 'Table', headerShown: false }}
-          />
-          <Stack.Screen
-            name="ExistDataset"
-            component={ExistDataset}
-            options={{ title: 'exist', headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View style={styles.container}>
+        <Image
+          style = {{height: 200, width: Dimensions.get('window').width - 40}}
+          source = {require('./assets/orchardWatch.png')}
+        />
+        <TextInput 
+          style = {styles.inputBox}
+          placeholder = 'Access Code'
+          secureTextEntry = {true}
+          keyboardType = 'number-pad'
+          textAlign = {'center'}
+          placeholderTextColor = {'gray'}
+          onChangeText = {(code)=>this.setState({code})}
+        />
+        <Button
+          style = {styles.submitButton}
+          title = 'Submit'
+          onPress = {()=>this.LoginHandler()}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#90EE90',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputBox: {
+    height: 40, 
+    width: Dimensions.get('window').width - 40, 
+    borderColor: 'black', 
+    borderWidth: 1,
+  },
+});
+
+AppRegistry.registerComponent('App',()=>App);
