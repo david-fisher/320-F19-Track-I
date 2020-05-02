5246281c0eae10ee31cdd861116e3d04968372be
Evanr35 <example@example.com>
2020-05-02 13:00:40 -0400

    Update Announcements.js

--- client/src/pages/Announcements/Announcements.js
+++ client/src/pages/Announcements/Announcements.js
@@ -1,35 +1,68 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Announcements.css";
 import { Container } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import TestData from "./testData.json"
 
 export function Announcements() {
+    const [data, setData] = useState({announcements: [], isFetching: false});
+    useEffect(() => {
+        const fetchAnnouncements = async () => {
+            try {
+                setData({announcements: data.announcements, isFetching: true});
+                fetch('https://2a2glx2h08.execute-api.us-east-2.amazonaws.com/default/announcements')
+                    .then(response => response.json())
+                    .then(result => {
+                        setData({
+                            announcements: result.map(content => ({
+                                num: content[0].longValue,
+                                title: content[1].stringValue,
+                                description: content[2].stringValue,
+                                date: content[3].stringValue,
+                            })), isFetching: false})
+                    })
+                    .catch(e => {
+                        console.log(e);
+                        setData({contacts: data.contacts, isFetching: false});
+                    });
+            } catch (e) {
+                console.log(e);
+                setData({announcements: data.announcements, isFetching: false});
+            }
+        };
+        fetchAnnouncements();
+    }, []);
+
   return (
-    <body>
+    <div>
+      <p id="demo"></p>
+
       <Container className="Title">
         <h1>Announcements</h1>
         <hr />
       </Container>
-      
-      <Container>
-		<img src={require("./announcements.jpg")} alt="Announcements" class="center" />        
 
-		<Button className="addUpdate">
-          Add Announcement
-        </Button>
+    <Container>
+    {email !== undefined && <Button className="addUpdate" href="/addannouncement">
+        Add Announcement
+    </Button>}
+    </Container>
+
       </Container>
-        
-      {TestData.map((annInfo, index)=> {
-        return <Container>
-          <h1 classname="title">{annInfo.title}</h1>
-          <p class="lead border border-light rounded">
-            {annInfo.date}: {annInfo.description}
-          </p>
-        	
-        </Container>
-      })}
-    </body>
+      {data.announcements.map((announcementsInfo, index)=> {
+          return <Container key={index}>
+              <h1 className="title">
+                {announcementsInfo.title}
+              </h1>
+              <p className="lead border border-light rounded">
+                {announcementsInfo.date}
+              </p>
+              <p className="lead border border-light rounded">
+                {announcementsInfo.description}
+              </p>
+          </Container>
+    })})}
+    </div>
   );
 }
 
